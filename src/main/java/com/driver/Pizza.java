fd36a0da31cb4d461b6232077f2128541dabc038
unknown <adeep0420@gmail.com>
2022-11-22 02:31:24 +0530

    Add billing pizza system

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -2,33 +2,87 @@ package com.driver;
 
 public class Pizza {
 
-    private int price;
+    private int price=0;
+
+    private int basePrice = 0;
     private Boolean isVeg;
-    private String bill;
+    private String bill = "";
+
+    private Boolean extraCheese = false;
+    private Boolean extraToppings = false;
+    private Boolean takeAway = false;
+
 
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
-        // your code goes here
+        if(isVeg == true) {
+            price += 300;
+            basePrice += 300;
+        }
+        else {
+            price += 400;
+            basePrice += 400;
+        }
     }
-
+    public void ifDeluxe(Boolean deluxe) {
+        if(isVeg == true) {
+            price += 80 + 70;
+            basePrice += 80 + 70;
+        }
+        else {
+            price += 80 + 120;
+            basePrice += 80 + 120;
+        }
+    }
     public int getPrice(){
         return this.price;
     }
 
+
     public void addExtraCheese(){
-        // your code goes here
+        if(extraCheese == false) {
+                price += 80;
+        }
+        extraCheese = true;
     }
 
     public void addExtraToppings(){
-        // your code goes here
+        if(extraToppings == false) {
+            if(isVeg == true)
+                price += 70;
+            else
+                price += 120;
+        }
+        extraToppings = true;
     }
 
     public void addTakeaway(){
-        // your code goes here
+        if(takeAway == false) {
+            price += 20;
+        }
+        takeAway = true;
     }
 
     public String getBill(){
-        // your code goes here
+        if(isVeg == true)
+            bill += "Base Price Of The Pizza: " + basePrice + "\n";
+            else
+            bill += "Base Price Of The Pizza: " + basePrice + "\n";
+
+        if(extraCheese == true)
+            bill += "Extra Cheese Added: 80 \n";
+
+        if(extraToppings == true) {
+            if(isVeg == true)
+                bill += "Extra Toppings Added: 70 \n";
+            else
+                bill += "Extra Toppings Added: 120 \n";
+        }
+
+        if(takeAway == true) {
+            bill += "Paperbag Added: 20 \n";
+        }
+        bill += "Total Price: "+String.valueOf(price)+"\n";
         return this.bill;
     }
 }
